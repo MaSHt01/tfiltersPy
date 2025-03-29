b11f397386e32632f74cfbd49d335454e98fa3e8
thabang sekgoto <realthabangvision@gmail.com>
2025-03-29 10:14:35 +0200

    ne ke rename dilo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,25 +1,18 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
 
-try:
-    # For Python 3.8 and above
-    from importlib.metadata import version
-except ImportError:
-    # For older versions of Python, use the backport
-    from importlib_metadata import version
-
-# Set the release variable dynamically using the package version.
-release = version("tfilterspy")
-
 # -- Project information -----------------------------------------------------
-project = 'tfilters'
-copyright = (
-    '2025, Thabang Mashinin-Sekhoto, '
-    'Lebogang Mashinini-Sekhoto, '
-    'Palesa Mashinini-Sekhoto'
-)
+project = 'tfilterspy'
+copyright = '2025, Thabang Mashinin-Sekhoto, \
+Lebogang Mashinini-Sekhoto, Palesa Mashinini-Sekhoto'
 author = 'Thabang Mashinin-Sekhoto, Lebogang Mashinini-Sekhoto, Palesa Mashinini-Sekhoto'
+release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 extensions = [
@@ -27,7 +20,7 @@ extensions = [
     'sphinx.ext.napoleon',          # Support for Google/NumPy-style docstrings
     'sphinx.ext.autodoc.typehints', # Add type hints to the documentation
     'sphinx.ext.viewcode',          # Link source code in the docs
-    'nbsphinx',                    # Renders Jupyter notebooks
+     'nbsphinx',                    # Add this line to use nbsphinx
 ]
 templates_path = ['_templates']
 exclude_patterns = []
