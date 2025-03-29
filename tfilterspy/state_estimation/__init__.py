4964a71f6ff9bcf59180e2a3943834daa4e7c4fc
Tha-bang! <45782148+LeparaLaMapara@users.noreply.github.com>
2025-03-29 09:56:52 +0200

    renamed

--- tfilterspy/state_estimation/__init__.py
+++ tfilterspy/state_estimation/__init__.py
@@ -6,4 +6,5 @@ from .linear_filters import DaskKalmanFilter
 from .nonlinear_filters import DaskNonLinearKalmanFilter
 from .particle_filters import DaskParticleFilter
 
+
 __all__ = ["DaskKalmanFilter", "DaskNonLinearKalmanFilter", "DaskParticleFilter"]
